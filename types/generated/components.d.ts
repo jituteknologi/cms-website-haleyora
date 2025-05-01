540967d81b874fe0c7caea80932e05067cdfae63
Wahyu Taufik <wahyu.taufik@sera.astra.co.id>
2025-05-01 19:14:45 +0700

    Dynamic sender and image whatsapp

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -11,6 +11,8 @@ export interface SharedWhatsapp extends Schema.Component {
     phone_number: Attribute.String & Attribute.Required;
     initial_message: Attribute.Text & Attribute.Required;
     display_message: Attribute.Text & Attribute.Required;
+    sender_name: Attribute.String & Attribute.Required;
+    icon_image: Attribute.Media<'images'> & Attribute.Required;
   };
 }
 
@@ -260,6 +262,64 @@ export interface ProfileCompanyValue extends Schema.Component {
   };
 }
 
+export interface OrganizationDirector extends Schema.Component {
+  collectionName: 'components_organization_directors';
+  info: {
+    displayName: 'director';
+    icon: 'shield';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    endpoint: Attribute.String & Attribute.Required;
+    params: Attribute.Component<'repeatable.params', true>;
+  };
+}
+
+export interface OrganizationCommissioner extends Schema.Component {
+  collectionName: 'components_organization_commissioners';
+  info: {
+    displayName: 'commissioner';
+    icon: 'shield';
+    description: '';
+  };
+  attributes: {
+    endpoint: Attribute.String & Attribute.Required;
+    params: Attribute.Component<'repeatable.params', true>;
+    title: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface MenuSubMenu extends Schema.Component {
+  collectionName: 'components_menu_sub_menus';
+  info: {
+    displayName: 'sub menu';
+    icon: 'filter';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    url: Attribute.String;
+    target: Attribute.Enumeration<['_blank', '_parent', '_self', '_top']>;
+    subtitle: Attribute.String;
+    icon: Attribute.String & Attribute.CustomField<'plugin::react-icons.icon'>;
+  };
+}
+
+export interface MenuMenu extends Schema.Component {
+  collectionName: 'components_menu_menus';
+  info: {
+    displayName: 'menu';
+    icon: 'layer';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    url: Attribute.String;
+    target: Attribute.Enumeration<['_blank', '_parent', '_top', '_self']>;
+    sub_menu: Attribute.Component<'menu.sub-menu', true>;
+  };
+}
+
 export interface ProcurementDocument extends Schema.Component {
   collectionName: 'components_procurement_documents';
   info: {
@@ -320,64 +380,6 @@ export interface ProcurementAnnouncementDoc extends Schema.Component {
   };
 }
 
-export interface OrganizationDirector extends Schema.Component {
-  collectionName: 'components_organization_directors';
-  info: {
-    displayName: 'director';
-    icon: 'shield';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    endpoint: Attribute.String & Attribute.Required;
-    params: Attribute.Component<'repeatable.params', true>;
-  };
-}
-
-export interface OrganizationCommissioner extends Schema.Component {
-  collectionName: 'components_organization_commissioners';
-  info: {
-    displayName: 'commissioner';
-    icon: 'shield';
-    description: '';
-  };
-  attributes: {
-    endpoint: Attribute.String & Attribute.Required;
-    params: Attribute.Component<'repeatable.params', true>;
-    title: Attribute.String & Attribute.Required;
-  };
-}
-
-export interface MenuSubMenu extends Schema.Component {
-  collectionName: 'components_menu_sub_menus';
-  info: {
-    displayName: 'sub menu';
-    icon: 'filter';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    url: Attribute.String;
-    target: Attribute.Enumeration<['_blank', '_parent', '_self', '_top']>;
-    subtitle: Attribute.String;
-    icon: Attribute.String & Attribute.CustomField<'plugin::react-icons.icon'>;
-  };
-}
-
-export interface MenuMenu extends Schema.Component {
-  collectionName: 'components_menu_menus';
-  info: {
-    displayName: 'menu';
-    icon: 'layer';
-    description: '';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    url: Attribute.String;
-    target: Attribute.Enumeration<['_blank', '_parent', '_top', '_self']>;
-    sub_menu: Attribute.Component<'menu.sub-menu', true>;
-  };
-}
-
 export interface InvestorSummary extends Schema.Component {
   collectionName: 'components_investor_summaries';
   info: {
@@ -403,123 +405,6 @@ export interface InvestorData extends Schema.Component {
         maxLength: 4;
       }>;
     value: Attribute.Integer & Attribute.Required;
-  };
-}
-
-export interface EvhcPromo extends Schema.Component {
-  collectionName: 'components_evhc_promos';
-  info: {
-    displayName: 'promo';
-    icon: 'landscape';
-  };
-  attributes: {
-    item: Attribute.Component<'evhc.promo-item', true>;
-  };
-}
-
-export interface EvhcPromoItem extends Schema.Component {
-  collectionName: 'components_evhc_promo_items';
-  info: {
-    displayName: 'promo item';
-    icon: 'bulletList';
-  };
-  attributes: {
-    banner: Attribute.Media<'images'> & Attribute.Required;
-    term_condition: Attribute.RichText &
-      Attribute.CustomField<
-        'plugin::ckeditor5.CKEditor',
-        {
-          preset: 'default';
-        }
-      >;
-  };
-}
-
-export interface EvhcProcedure extends Schema.Component {
-  collectionName: 'components_evhc_procedures';
-  info: {
-    displayName: 'procedure';
-    icon: 'bulletList';
-    description: '';
-  };
-  attributes: {
-    procedure_item: Attribute.Component<'evhc.procedure-item', true>;
-  };
-}
-
-export interface EvhcProcedureItem extends Schema.Component {
-  collectionName: 'components_evhc_procedure_items';
-  info: {
-    displayName: 'procedure item';
-    icon: 'bulletList';
-    description: '';
-  };
-  attributes: {
-    icon: Attribute.String &
-      Attribute.Required &
-      Attribute.CustomField<'plugin::react-icons.icon'>;
-    label: Attribute.String & Attribute.Required;
-    description: Attribute.Text;
-  };
-}
-
-export interface EvhcIncentive extends Schema.Component {
-  collectionName: 'components_evhc_incentives';
-  info: {
-    displayName: 'incentive';
-    icon: 'layout';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    incentive_list: Attribute.Component<'evhc.incentive-list', true> &
-      Attribute.Required;
-    link: Attribute.Component<'shared.cta-button'>;
-  };
-}
-
-export interface EvhcIncentiveList extends Schema.Component {
-  collectionName: 'components_evhc_incentive_lists';
-  info: {
-    displayName: 'incentive list';
-    icon: 'bulletList';
-  };
-  attributes: {
-    item: Attribute.String & Attribute.Required;
-  };
-}
-
-export interface EvhcFaq extends Schema.Component {
-  collectionName: 'components_evhc_faqs';
-  info: {
-    displayName: 'faq';
-    icon: 'bulletList';
-  };
-  attributes: {
-    item: Attribute.Component<'evhc.faq-item', true>;
-  };
-}
-
-export interface EvhcFaqItem extends Schema.Component {
-  collectionName: 'components_evhc_faq_items';
-  info: {
-    displayName: 'faq item';
-    icon: 'bulletList';
-  };
-  attributes: {
-    question: Attribute.String & Attribute.Required;
-    answer: Attribute.Text;
-  };
-}
-
-export interface EvhcCompatibility extends Schema.Component {
-  collectionName: 'components_evhc_compatibilities';
-  info: {
-    displayName: 'compatibility';
-    icon: 'landscape';
-    description: '';
-  };
-  attributes: {
-    brand_logo: Attribute.Media<'images'> & Attribute.Required;
   };
 }
 
@@ -678,6 +563,123 @@ export interface HomeAchievement extends Schema.Component {
   };
 }
 
+export interface EvhcPromo extends Schema.Component {
+  collectionName: 'components_evhc_promos';
+  info: {
+    displayName: 'promo';
+    icon: 'landscape';
+  };
+  attributes: {
+    item: Attribute.Component<'evhc.promo-item', true>;
+  };
+}
+
+export interface EvhcPromoItem extends Schema.Component {
+  collectionName: 'components_evhc_promo_items';
+  info: {
+    displayName: 'promo item';
+    icon: 'bulletList';
+  };
+  attributes: {
+    banner: Attribute.Media<'images'> & Attribute.Required;
+    term_condition: Attribute.RichText &
+      Attribute.CustomField<
+        'plugin::ckeditor5.CKEditor',
+        {
+          preset: 'default';
+        }
+      >;
+  };
+}
+
+export interface EvhcProcedure extends Schema.Component {
+  collectionName: 'components_evhc_procedures';
+  info: {
+    displayName: 'procedure';
+    icon: 'bulletList';
+    description: '';
+  };
+  attributes: {
+    procedure_item: Attribute.Component<'evhc.procedure-item', true>;
+  };
+}
+
+export interface EvhcProcedureItem extends Schema.Component {
+  collectionName: 'components_evhc_procedure_items';
+  info: {
+    displayName: 'procedure item';
+    icon: 'bulletList';
+    description: '';
+  };
+  attributes: {
+    icon: Attribute.String &
+      Attribute.Required &
+      Attribute.CustomField<'plugin::react-icons.icon'>;
+    label: Attribute.String & Attribute.Required;
+    description: Attribute.Text;
+  };
+}
+
+export interface EvhcIncentive extends Schema.Component {
+  collectionName: 'components_evhc_incentives';
+  info: {
+    displayName: 'incentive';
+    icon: 'layout';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    incentive_list: Attribute.Component<'evhc.incentive-list', true> &
+      Attribute.Required;
+    link: Attribute.Component<'shared.cta-button'>;
+  };
+}
+
+export interface EvhcIncentiveList extends Schema.Component {
+  collectionName: 'components_evhc_incentive_lists';
+  info: {
+    displayName: 'incentive list';
+    icon: 'bulletList';
+  };
+  attributes: {
+    item: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface EvhcFaq extends Schema.Component {
+  collectionName: 'components_evhc_faqs';
+  info: {
+    displayName: 'faq';
+    icon: 'bulletList';
+  };
+  attributes: {
+    item: Attribute.Component<'evhc.faq-item', true>;
+  };
+}
+
+export interface EvhcFaqItem extends Schema.Component {
+  collectionName: 'components_evhc_faq_items';
+  info: {
+    displayName: 'faq item';
+    icon: 'bulletList';
+  };
+  attributes: {
+    question: Attribute.String & Attribute.Required;
+    answer: Attribute.Text;
+  };
+}
+
+export interface EvhcCompatibility extends Schema.Component {
+  collectionName: 'components_evhc_compatibilities';
+  info: {
+    displayName: 'compatibility';
+    icon: 'landscape';
+    description: '';
+  };
+  attributes: {
+    brand_logo: Attribute.Media<'images'> & Attribute.Required;
+  };
+}
+
 export interface CoverageItem extends Schema.Component {
   collectionName: 'components_coverage_items';
   info: {
@@ -740,25 +742,16 @@ declare module '@strapi/types' {
       'profile.history': ProfileHistory;
       'profile.history-item': ProfileHistoryItem;
       'profile.company-value': ProfileCompanyValue;
-      'procurement.document': ProcurementDocument;
-      'procurement.classification': ProcurementClassification;
-      'procurement.announcement-period': ProcurementAnnouncementPeriod;
-      'procurement.announcement-doc': ProcurementAnnouncementDoc;
       'organization.director': OrganizationDirector;
       'organization.commissioner': OrganizationCommissioner;
       'menu.sub-menu': MenuSubMenu;
       'menu.menu': MenuMenu;
+      'procurement.document': ProcurementDocument;
+      'procurement.classification': ProcurementClassification;
+      'procurement.announcement-period': ProcurementAnnouncementPeriod;
+      'procurement.announcement-doc': ProcurementAnnouncementDoc;
       'investor.summary': InvestorSummary;
       'investor.data': InvestorData;
-      'evhc.promo': EvhcPromo;
-      'evhc.promo-item': EvhcPromoItem;
-      'evhc.procedure': EvhcProcedure;
-      'evhc.procedure-item': EvhcProcedureItem;
-      'evhc.incentive': EvhcIncentive;
-      'evhc.incentive-list': EvhcIncentiveList;
-      'evhc.faq': EvhcFaq;
-      'evhc.faq-item': EvhcFaqItem;
-      'evhc.compatibility': EvhcCompatibility;
       'home.slide': HomeSlide;
       'home.services': HomeServices;
       'home.service-item': HomeServiceItem;
@@ -769,6 +762,15 @@ declare module '@strapi/types' {
       'home.company-description': HomeCompanyDescription;
       'home.cis': HomeCis;
       'home.achievement': HomeAchievement;
+      'evhc.promo': EvhcPromo;
+      'evhc.promo-item': EvhcPromoItem;
+      'evhc.procedure': EvhcProcedure;
+      'evhc.procedure-item': EvhcProcedureItem;
+      'evhc.incentive': EvhcIncentive;
+      'evhc.incentive-list': EvhcIncentiveList;
+      'evhc.faq': EvhcFaq;
+      'evhc.faq-item': EvhcFaqItem;
+      'evhc.compatibility': EvhcCompatibility;
       'coverage.item': CoverageItem;
       'coverage.head-office': CoverageHeadOffice;
       'coverage.affiliation': CoverageAffiliation;
